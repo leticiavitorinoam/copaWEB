d9818649ee28a9049f680ca13177bc691a10e8fa
leticiavitorinoam <leticiavitorinoam@gmail.com>
2026-06-30 23:32:13 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -31,9 +31,13 @@ def classificacao():
     response = requests.get(f"{BASE_URL}/standings?league=1&season=2022", headers=headers)
     return jsonify(response.json())
 
-@app.route("/api/classificacao/<data>")
-def classificacao_por_data(data):
-    response = requests.get(f"{BASE_URL}/standings?league=1&season=2022&date={data}", headers=headers).json()
+@app.route("/api/classificacao")
+def classificacao():
+    response = requests.get(
+        f"{BASE_URL}/standings",
+        headers=headers,
+        params={"league": 1, "season": 2022}
+    ).json()
     return jsonify(response["response"])
 
 @app.route("/api/artilheiros")
@@ -43,3 +47,53 @@ def artilheiros():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+
+
+=====================================================
+
+#import das bibliotecas do Flask
+from flask import Flask, jsonify
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv() #lê o arquivo .env
+
+app = Flask(__name__) #vai criar o servidor
+
+API_KEY = os.getenv("API_KEY") #guarda a chave 
+BASE_URL = "https://v3.football.api-sports.io"
+
+headers = {"x-apisports-key": API_KEY}
+
+@app.route("/api/")
+
+@app.route("/api/selecoes")
+def selecoes():
+    response = requests.get(
+        f"{BASE_URL}/teams",
+        headers=headers,
+        params={"league": 1, "season": 2022}
+    ).json()
+    return jsonify(response["response"])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+    #estrutura:
+        #@app.route("/caminho/da/rota")
+        #def nome_da_funcao():
+            # 1. faz a requisição pra API
+            #response = requests.get(f"{BASE_URL}/endpoint", headers=headers, params={...}).json()
+    
+            # 2. retorna os dados como JSON pro front
+            #return jsonify(response["response"])
+        
