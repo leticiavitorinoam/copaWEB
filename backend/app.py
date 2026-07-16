3bebab735bc83703f36d6cc8a4e12a35ba81df6a
leticiavitorinoam <leticiavitorinoam@gmail.com>
2026-07-16 00:15:45 -0300

    arrumando para deploy

--- backend/app.py
+++ backend/app.py
@@ -263,4 +263,5 @@ def jogadores_partida(fixture):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
